6ae2a965b8d2550c0634af507267a3ba9335c8ba
Fedja Bogataj <91506705+fedjabogataj@users.noreply.github.com>
2026-04-15 00:08:35 +0200

    Removed overscroll effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-black`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-black overscroll-none`}
     >
       <body className="min-h-full flex flex-col bg-black">
         {children}
